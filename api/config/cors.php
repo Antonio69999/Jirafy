b440b4f933a5e98e334291d40408d9ae25af7338
achabeauti <achabeauti@capdigit.fr>
2025-09-24 14:10:23 +0200

    help

--- api/config/cors.php
+++ api/config/cors.php
@@ -3,7 +3,8 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'jirafyapi.garage404.com', // Votre domaine client
+        'jirafyapi.garage404.com',
+        'https://jirafy.garage404.com', // Votre domaine client
         'http://localhost:5173', // Pour le dev local
         'http://localhost:5175', // Votre port Vite
     ],
